e942610eb1a340326f4fe3891386eeecf081b646
Shivani2825 <jainshivani285@gmail.com>
2022-12-24 16:31:03 +0530

    email verification done

--- emailsending/emailsending/settings.py
+++ emailsending/emailsending/settings.py
@@ -113,6 +113,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "shivani285jain"
+EMAIL_HOST_USER = "ENTER YOUR EMAIL"
 EMAIL_HOST_PASSWORD = 'pmmnysfdbnohewek'
     
